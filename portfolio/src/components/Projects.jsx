49097d3f1998ee1e2c23bfccc7783b00a5bd1129
colleenalicea <aliceacolleen3@gmail.com>
2025-03-05 16:15:19 -0500

    Fixed Component

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,9 +1,12 @@
-export default function Projects() {
+import React from 'react';
+
+function Projects() {
     return (
-        <div>
-            <h1>
-                Projects Page
-            </h1>
+        <div className="projects-page">
+            <h1 className="projects-title">Projects Page</h1>
+            {/* Put images here */}
         </div>
-    )
+    );
 }
+
+export default Projects;
