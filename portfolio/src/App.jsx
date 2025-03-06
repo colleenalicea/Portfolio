4773ef24ef2dba080cd84151ff5c32a7eb3b72f1
colleenalicea <aliceacolleen3@gmail.com>
2025-03-06 17:01:06 -0500

    Skills component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,30 +1,23 @@
-import React from 'react';
+import React from "react";
 // import Projects from './components/Projects';
 // import ProjectsBtn from './components/projectsbtn';
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
 // import Page from './components/pageprojects/Page';
-import AboutMe from './components/Aboutme';
-
-const computerLogo = '/computerlogo.png';
+import AboutMe from "./components/Aboutme";
+import Skills from "./components/Skills";
+const computerLogo = "/computerlogo.png";
 
 function App() {
-    return (
-        <div className="App">
-            <Header  icon='{faPhone, faEnvelope, faMapMarkerAlt}' img={computerLogo}   />
-            <AboutMe />
-            {/* <ProjectsBtn/> */}
-            {/* <Router>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/projects" element={<Projects
-    Btn />} />
-     </Routes>
-     </Router> */}
-       
-          
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header icon="{faPhone, faEnvelope, faMapMarkerAlt}" img={computerLogo} />
+      
+        <AboutMe />
+        <Skills />
+      
+    </div>
+  );
 }
 
 export default App;
